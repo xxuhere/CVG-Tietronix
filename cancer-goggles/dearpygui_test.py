ee829c3cf2770d655f7e9281d8a4894012484fdb
Fangchen Li <fangchen.li@outlook.com>
2021-02-14 12:01:14 -0600

    pace the render loop with the camera frame rate

--- cancer-goggles/dearpygui_test.py
+++ cancer-goggles/dearpygui_test.py
@@ -5,6 +5,7 @@ import cv2
 from models import Camera
 
 
+# camera_number = "rtsp://10.0.0.7:8555/unicast"
 camera_number = 0
 
 resolution = (640, 480)
@@ -16,12 +17,24 @@ camera = Camera(camera_number, resolution, fourcc, fps, timestamped=timestamped)
 
 c = 100
 
+frame_target = 4
+frame_counter = 0
+
 
 def update_canvas(sender, data):
-    frame = camera.get_frame()
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2RGBA)
-    dpg.add_texture("texture id", frame, 640, 480)
-    dpg.draw_image('drawing', "texture id", [0, 0], [640, 480], uv_min=[0, 0], uv_max=[1, 1])
+    global frame_counter, frame_target
+    if frame_counter == frame_target:
+        dpg.log_info("update frame")
+        frame = camera.get_frame()
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2RGBA)
+        dpg.add_texture("texture id", frame, 640, 480)
+        dpg.draw_image('drawing', "texture id", [0, 0], [640, 480], uv_min=[0, 0], uv_max=[1, 1])
+        frame_counter = 0
+    else:
+        frame_counter += 1
+
+
+dpg.show_logger()
 
 
 with sdpg.window("Video Window", width=640, height=480):
@@ -29,7 +42,6 @@ with sdpg.window("Video Window", width=640, height=480):
     dpg.set_main_window_title("Pixel selector")
 
     dpg.add_drawing('drawing', width=640, height=480)
-    dpg.add_label_text("FPS", default_value="0")
 
 
 dpg.set_render_callback(update_canvas)
