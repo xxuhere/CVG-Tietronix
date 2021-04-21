18d4d90c55052bc6605f3faef5e8a4337bfa5a94
Fangchen Li <fangchen.li@outlook.com>
2021-04-21 16:31:50 -0500

    vender yen's threshold method from scikit-image

--- src/algorithms/threshold.py
+++ src/algorithms/threshold.py
@@ -1,6 +1,29 @@
+# part of this file was taken from scikit-image
+
 import cv2
 import numpy as np
-from skimage.filters import threshold_yen
+
+from src.algorithms.utils import _validate_image_histogram
+
+
+def threshold_yen(image=None, nbins=256, *, hist=None):
+    counts, bin_centers = _validate_image_histogram(image, hist, nbins)
+
+    # On blank images (e.g. filled with 0) with int dtype, `histogram()`
+    # returns ``bin_centers`` containing only one value. Speed up with it.
+    if bin_centers.size == 1:
+        return bin_centers[0]
+
+    # Calculate probability mass function
+    pmf = counts.astype(np.float32) / counts.sum()
+    P1 = np.cumsum(pmf)  # Cumulative normalized histogram
+    P1_sq = np.cumsum(pmf ** 2)
+    # Get cumsum calculated from end of squared array:
+    P2_sq = np.cumsum(pmf[::-1] ** 2)[::-1]
+    # P2_sq indexes is shifted +1. I assume, with P1[:-1] it's help avoid
+    # '-inf' in crit. ImageJ Yen implementation replaces those values by zero.
+    crit = np.log(((P1_sq[:-1] * P2_sq[1:]) ** -1) * (P1[:-1] * (1.0 - P1[:-1])) ** 2)
+    return bin_centers[crit.argmax()]
 
 
 def thresholding(image: np.ndarray):
