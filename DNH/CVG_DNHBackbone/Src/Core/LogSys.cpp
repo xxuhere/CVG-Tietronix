5fc8a4862d41b3f6556d9efef7ea15dd5f5a0864
wleuTietronix <william.leu@tietronix.com>
2021-11-01 19:48:39 -0500

    Testing fix to cout freeze issue
    It's hard to track down (probably related to threading) so we may
    not know if it immediately works.

--- DNH/CVG_DNHBackbone/Src/Core/LogSys.cpp
+++ DNH/CVG_DNHBackbone/Src/Core/LogSys.cpp
@@ -1,12 +1,21 @@
 #include "LogSys.h"
 #include <iostream>
-
+#include <mutex>
 namespace CVG
 {
 	void LogSys::Log(const std::string& message)
 	{
+		// It's been observed that the app will freeze sometimes
+		// when trying to push content to std::cout. The current
+		// theory is that 
+		// << std::endl 
+		// will flush the buffer which is somehow the culprit when
+		// mixed with the issue of threading.
+		static std::mutex coutMutex;
+		std::lock_guard<std::mutex> guard(coutMutex);
+
 		// Logging implementation here
-		std::cout << message << std::endl;
+		std::cout << (message + "\n");// << std::endl;
 	}
 
 	void LogSys::LogVerbose(const std::string& message)
