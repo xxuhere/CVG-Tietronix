898694f2ea886949b9655fb00fa8252120e96b61
Paz Zait-Givon <paz.zait-givon@tietronix.com>
2022-04-25 15:30:32 -0500

    minor documentation cleanup VideoPollType to ProcessingType

--- DNH/HMDOpView/Utils/ProcessingType.h
+++ DNH/HMDOpView/Utils/ProcessingType.h
@@ -21,23 +21,23 @@ enum class ProcessingType {
 };
 
 /// <summary>
-/// Convert a VideoPollType to a serialiable string value.
+/// Convert a ProcessingType to a serialiable string value.
 /// 
 /// The name convention is made to match std::to_string() functions.
 /// </summary>
-/// <param name="ty">The VideoPollType to get the name of.</param>
+/// <param name="ty">The ProcessingType to get the name of.</param>
 /// <returns>
-/// A serializable string version of VideoPollType that can be converted
-/// back with StringToPollType().
+/// A serializable string version of ProcessingType that can be converted
+/// back with ProcessingType().
 /// </returns>
 std::string to_string(ProcessingType ty);
 
 /// <summary>
-/// Convert a VideoPollType to a serialiable string value.
+/// Convert a ProcessingType to a serialiable string value.
 /// </summary>
-/// <param name="ty">TThe name of a VideoPollType to convert to an enum.</param>
+/// <param name="ty">TThe name of a ProcessingType to convert to an enum.</param>
 /// <returns>
-/// An enum version of a VideoPollType string. If the string is not recognized,
-/// it is defaulted to VideoPollType::Deactivated.
+/// An enum version of a ProcessingType string. If the string is not recognized,
+/// it is defaulted to ProcessingType::None.
 /// </returns>
 ProcessingType StringToProcessingType(const std::string& str);
