57eb01201d6e2412481846290fd181c8e120bdc5
wleuTietronix <wleu@tietronix.com>
2022-10-19 18:22:42 -0500

    Script to reconnect USB2BT and start hmdopapp

---
+++ expect_connect.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/expect -f
+
+# This function is used in StartWBluetooth.sh. Because of how bash and 
+# expect work, this script file needs to be separate.
+set timeout 60
+
+send -- "echo Running program to wait until USB2BT is scanned and found in an expect shell"
+
+spawn bluetoothctl
+expect "Agent registered"
+send -- "scan on\rdevices\r"
+expect "*USB2BT*"
+send -- "exit\r"
+expect eof
